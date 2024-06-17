db21f6bc44bc3d5c5a77e2d74ef094a3e7d2a35e
Vukan Sukara <vukan.sukara@jsguru.io>
2024-06-17 14:55:26 +0200

    added employers, tags, jobs

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,22 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
-        <x-employer-logo />
+        <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <a href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{$job->title}}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
     </div>
 
     <div>
         <div>
-            <x-tag>Tags</x-tag>
-            <x-tag>Tags</x-tag>
-            <x-tag>Tags</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag/>
+            @endforeach
+
         </div>
     </div>
 </x-panel>
